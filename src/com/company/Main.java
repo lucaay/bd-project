010eba3a1963dbc010c1d8555ee2b82e5bba48f7
Dinu Neculai <nicu.dinu.neculai@gmail.com>
2021-10-26 22:29:38 +0300

    Am adaugat afisarea claselor in main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,8 +16,27 @@ public class Main {
         capacitate.add(24);
         capacitate.add(46);
         Frigider f=new Frigider(2.3f,"Hsf sfs23","Valabil","Arctic","⭐⭐⭐⭐⭐","In perete","Amoled","A+++","Fara","Fara",functii,capacitate);
-        System.out.println(f);
+        System.out.println(f+"\n");
 
+      Lada_Frigorifica lf = new Lada_Frigorifica();
+        System.out.println(lf+"\n");
 
+      Cafetiera_cu_filtru n=new Cafetiera_cu_filtru();
+        System.out.println(n+"\n");
+
+      Espressor es=new Espressor();
+        System.out.println(es + "\n");
+
+      Cuptoare_Electrice cup=new Cuptoare_Electrice();
+        System.out.println(cup + "\n");
+
+      Cuptoare_Microunde cupm= new Cuptoare_Microunde();
+        System.out.println(cupm + "\n");
+
+      Masina_de_Spalat_Rufe ms= new Masina_de_Spalat_Rufe();
+        System.out.println(ms + "\n");
+
+      Masina_de_Spalat_Vase msv = new Masina_de_Spalat_Vase();
+        System.out.println(msv + "\n");
     }
   }
