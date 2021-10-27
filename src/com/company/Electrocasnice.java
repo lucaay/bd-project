8f10731672195f77b8bdb9d89837991e6e81fd74
Antonie02 <sogaantonie3@gmail.com>
2021-10-27 10:37:25 +0300

    midificare in clasa electrocasnice, punem variabilele comune in clasa parinte

--- src/com/company/Electrocasnice.java
+++ src/com/company/Electrocasnice.java
@@ -1,8 +1,13 @@
 package com.company;
-//import java.util.Vector;
+import java.util.Vector;
 
 //import java.util.List;
 
 public class Electrocasnice {
-
+    /*private String tip_electrocasnice;
+    private float pret;
+    private String model;
+    private String brand;
+    private String rating;
+    private Vector<Integer> capacitate;*/
 }
